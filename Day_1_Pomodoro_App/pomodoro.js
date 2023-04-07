4014e23ac719173dc88452adce5559b9a38c1613
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-08 00:25:01 +0530

    timer running minutes display not configured

--- Day_1_Pomodoro_App/pomodoro.js
+++ Day_1_Pomodoro_App/pomodoro.js
@@ -2,6 +2,7 @@ let setting = document.getElementById('setting');
 let minutes = document.getElementById('minutes');
 let sec = document.getElementById('sec');
 
+console.log(VarSec);
 function ChangeTime(){
     console.log("start")
     minutes.toggleAttribute("disabled");
@@ -11,16 +12,26 @@ function ChangeTime(){
 function StartTimer(){
     let text= document.getElementById("text")
     if(  text.innerText == "START"){
+
+        let varsec = document.getElementById('sec').value;
+        let varmin  = document.getElementById('minutes').value;
+
+        let x= parseInt(varmin)*60 +parseInt(varsec);
+
+        console.log(varsec);
+
         text.innerText = "Pause";
-        let x=10;
         const timer = setInterval(()=>{
-            console.log(x);
-            if(x>0){
+            varsec= x;
+            if(x>1){
+                console.log(x);
                 x--;
+                document.getElementById('sec').value--;
             }
             else{
                 alert("Times Up!");
                 clearInterval(timer);
+                document.getElementById('sec').value =x;
             }
         },1000)
         // console.log("start")
