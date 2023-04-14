02e5f5e0622e2318e81e157f56d5ef1049ad0924
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-14 10:47:37 +0530

    Working clock

--- Day_1_Pomodoro_App/pomodoro.js
+++ Day_1_Pomodoro_App/pomodoro.js
@@ -2,41 +2,70 @@ let setting = document.getElementById('setting');
 let minutes = document.getElementById('minutes');
 let sec = document.getElementById('sec');
 
-console.log(VarSec);
-function ChangeTime(){
-    console.log("start")
+// console.log(VarSec);
+function ChangeTime() {
+    // console.log("start")
     minutes.toggleAttribute("disabled");
     sec.toggleAttribute("disabled");
 }
 
-function StartTimer(){
-    let text= document.getElementById("text")
-    if(  text.innerText == "START"){
+function StartTimer() {
+    // disable editing
+    ChangeTime();
+    // change color to default 
+    let r = document.querySelector(":root");
+    r.style.setProperty('--clock-color', '#46e646');
 
-        let varsec = document.getElementById('sec').value;
-        let varmin  = document.getElementById('minutes').value;
+    let text = document.getElementById("text")
+    if (text.innerText == "START") {
 
-        let x= parseInt(varmin)*60 +parseInt(varsec);
+        let varsec = parseInt(document.getElementById('sec').value);
+        let varmin = parseInt(document.getElementById('minutes').value);
 
-        console.log(varsec);
+        
 
         text.innerText = "Pause";
-        const timer = setInterval(()=>{
-            varsec= x;
-            if(x>1){
-                console.log(x);
-                x--;
-                document.getElementById('sec').value--;
+        const timer = setInterval(() => {
+
+            if (varsec > 0 || varmin > 0) {
+                // console.log(varmin,varsec,document.getElementById('sec').value);
+                // document.getElementById('sec').value.padStart(2,"0");
+
+                if (varmin > 0 && varsec == 0) {
+
+                    varsec = 59;
+                    varmin--;
+                    document.getElementById('sec').value = 59;
+                    document.getElementById('minutes').value--;
+
+                }
+                else if (varmin == 0 && varsec > 0) {
+                    varsec--;
+                    document.getElementById('sec').value--;
+
+                }
+                else {
+                    varsec--;
+                    document.getElementById('sec').value--;
+
+                }
+                // console.log(document.getElementById('sec').value.padStart(2,"0"));
+                // document.getElementById('sec').value.padStart(2,"0");
             }
-            else{
+            else {
+                // changing color 
+                let r = document.querySelector(":root");
+                r.style.setProperty('--clock-color', '#9c0f17');
+                // enable editing
+                ChangeTime();
                 alert("Times Up!");
                 clearInterval(timer);
-                document.getElementById('sec').value =x;
+                text.innerText = "Start"
             }
-        },1000)
+        }, 1000)
         // console.log("start")
     }
-    else if(text.innerText == "PAUSE"){
+    else if (text.innerText == "PAUSE") {
         text.innerText = "Start";
         // console.log("Pause");
     }
