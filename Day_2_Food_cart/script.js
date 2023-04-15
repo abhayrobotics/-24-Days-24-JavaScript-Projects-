fc6e90e660c7ce81e6ad305e1dc4d55353e9bc68
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-16 01:19:56 +0530

    object and design ready for day 2

---
+++ Day_2_Food_cart/script.js
@@ -0,0 +1,45 @@
+let cart = {
+    french: {
+        name:"French Fries with Ketchup",
+        url: "./asset/plate__french-fries.png",
+        quantity: 0,
+        price: 110.,
+    },
+    salmon: {
+        name:"Salmon and Vegetables",
+        url:"./asset/plate__salmon-vegetables.png",
+        quantity: 0,
+        price: 195.5,
+    },
+    spaghetti: {
+        name:"Spaghetti Meat & Sauce",
+        url:"./asset/plate__spaghetti-meat-sauce.png",
+        quantity: 0,
+        price: 250.00
+    },
+    chicken: {
+        name:">Chicken Salad",
+        url:"./asset/plate__chicken-salad.png",
+        quantity: 0,
+        price: 215.00,
+    },
+    bacon: {
+        name:"Bacon Eggs",
+        url:"./asset/plate__bacon-eggs.png",
+        quantity: 0,
+        price: 200.00
+    },
+    fish: {
+        name:"Fish Sticks Fries",
+        url:"./asset/plate__fish-sticks-fries.png",
+        quantity: 0,
+        price: 420.00,
+    },
+    ravioli: {
+        name:"Ravioli",
+        url:"./asset/plate__ravioli.png",
+        quantity: 0,
+        price: 315.00,
+    },
+}
+console.log(cart.french.price);
