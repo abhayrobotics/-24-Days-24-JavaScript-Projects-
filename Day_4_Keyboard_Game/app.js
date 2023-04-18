9987c98717f0d1020d21b289aa7377de2f7a1b03
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-19 00:28:39 +0530

    Keyboard game working

--- Day_4_Keyboard_Game/app.js
+++ Day_4_Keyboard_Game/app.js
@@ -1,37 +1,40 @@
-
-let status1 = true;
-if (status1) {
-    RandomGen()
-}
+let score = 0;
 function RandomGen() {
+    console.log("random func call");
     status1 = false;
     let arr = document.querySelectorAll(".key");
     // generating random inter no.
     const random = Math.floor(Math.random() * arr.length);
 
-    // console.log(arr[random].innerText);
     // highlighting
     arr[random].classList.toggle("jiggle");
 
+    // InputFunc()
+    // return random;
+}
+// let random = RandomGen()
+// function InputFunc(){
+    console.log("input func call");
     // UNDERSTANING THE KEY PRESSSED
-    document.addEventListener('keydown', function (event) {
-        const key2 = event.key;
+    document.addEventListener('keyup', function (event) {
+        const key2 = (event.key).toUpperCase();
         console.log(key2)
-        // check the prsesed key 
-        if (arr[random].getAttribute("data-key") == key2.toUpperCase()) {
-            console.log("correct");
-            arr[random].classList.toggle("jiggle");
+        // check the prsesed key
+        const jiggle = document.querySelector(".jiggle") ;
+        if (jiggle.getAttribute("data-key") === key2) {
+            score=+1;
+            console.log("correct",score);
+            jiggle.classList.toggle("jiggle");
             RandomGen();
             // setTimeout(RandomGen(),2000)
 
         } else {
-            console.log("wrong");
-            console.log("input", event.key);
-            console.log("random", arr[random].getAttribute("data-key"));
-
+            score=-1;
+            console.log("wrong",score);
         }
     })
 
-}
+// }
 
+RandomGen();
 
