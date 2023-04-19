b964531aa40afa006c872a9c7a828e413aa37e76
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-19 23:03:50 +0530

    keyboard game ready

--- Day_4_Keyboard_Game/app.js
+++ Day_4_Keyboard_Game/app.js
@@ -1,6 +1,7 @@
 
+
 function RandomGen() {
-    console.log("random func call");
+    // console.log("random func call");
     status1 = false;
     let arr = document.querySelectorAll(".key");
     // generating random inter no.
@@ -8,49 +9,30 @@ function RandomGen() {
 
     // highlighting
     arr[random].classList.toggle("jiggle");
-
-
 }
 
 
 // UNDERSTANING THE KEY PRESSSED
 document.addEventListener('keyup', function (event) {
     const key2 = (event.key).toUpperCase();
-    // console.log(key2)
-
-    // Score
-    // let scoreElement = document.getElementById('score').innerText;
-    let score;
-    if (sessionStorage.getItem("Score") == undefined) {
-        sessionStorage.setItem("Score", 0);
-       score =0;
-    } 
+   
 
     // check the prsesed key
     const jiggle = document.querySelector(".jiggle");
     if (jiggle.getAttribute("data-key") === key2) {
-        score = score + 1;
-        console.log("correct", score);
-        sessionStorage.setItem("Score", score);
-
-        document.getElementById('score').innerText = score;
+       
         jiggle.classList.toggle("jiggle");
         RandomGen();
-        // setTimeout(RandomGen(),2000)
-
-    } else {
+    }
+     else {
         score = score - 1;
-        console.log("wrong", score);
+        console.log("wrong", score, typeof score);
         sessionStorage.setItem("Score", score);
 
         document.getElementById('score').innerText = score
 
     }
-
-    
 })
-
-// }
 
 RandomGen();
 
