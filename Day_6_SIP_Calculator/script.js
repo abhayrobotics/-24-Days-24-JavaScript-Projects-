1a5d7056045e9545f60dbb5e0df0e3ffd3e1c632
abhaygupt <abhayrobotics1908@gmail.com>
2023-04-23 01:49:12 +0530

    final deploynebt sip calcc

--- Day_6_SIP_Calculator/script.js
+++ Day_6_SIP_Calculator/script.js
@@ -122,65 +122,58 @@ function createBarChart() {
 
     // line chart array
     // let result = principal * ((((1 + i) ** n) - 1) / i) * (1 + i);
-    let label1 =[]
+    let label1 = []
     let invested = [];
-    let total =[];
-    for(let i= 0;i<=x[2];i=i+12){
-        label1.push(`Year ${i/12}`)
-        invested.push(x[0]*i);
+    let total = [];
+    for (let i = 0; i <= x[2]; i = i + 12) {
+        label1.push(`Year ${i / 12}`)
+        invested.push(x[0] * i);
         let eachYear = x[0] * ((((1 + x[1]) ** i) - 1) / x[1]) * (1 + x[1]);
         total.push(eachYear)
 
     }
-    console.log(label1,invested,total)
+    console.log(label1, invested, total)
     const labels = label1;
     let config = {
         type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: 'Amount Invested',
-                data: invested,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    // 'rgba(255, 159, 64, 1)',
-                   
-                ],
-                borderColor: [
-                    'rgb(255, 99, 132)',
-                    // 'rgb(255, 159, 64)',
-                   
-                ],
-                borderWidth: 1
-            },{
-                label: 'Total Value',
-                data: total,
-                backgroundColor: [
-                
-                    'rgba(255, 159, 64, 1)',
-                    // 'rgba(255, 205, 86, 1)',
-                    // 'rgba(75, 192, 192, 1)',
-                  
-                ],
-                borderColor: [
-                   
-                    'rgb(255, 159, 64)',
-                    // 'rgb(255, 205, 86)',
-                    // 'rgb(75, 192, 192)',
-                    // 'rgb(54, 162, 235)',
-                    // 'rgb(153, 102, 255)',
-                    // 'rgb(201, 203, 207)'
-                ],
-                borderWidth: 1
-            
-            }]
+            datasets: [
+                {
+                    label: 'Amount Invested',
+                    data: invested,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        // 'rgba(255, 159, 64, 1)',
+
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+
+                    ],
+                    borderWidth: 1,
+                },
+                {
+                    label: 'Total Value',
+                    data: total,
+                    backgroundColor: [
+
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderColor: [
+
+                        'rgb(255, 159, 64)',
+                    ],
+                    borderWidth: 1
+
+                }]
         },
         options: {
-            layout:{
-                autopadding:true,
+            layout: {
+                autopadding: true,
             },
             plugins: {
-                responsive:true,
+                responsive: true,
                 legend: {
                     display: true,
                     labels: {
@@ -188,12 +181,13 @@ function createBarChart() {
                     }
                 }
             },
+            
             // responsive:true,
         }
     }
     const mychart = new Chart(ctx, config);
 
 
-    }
+}
 
 
