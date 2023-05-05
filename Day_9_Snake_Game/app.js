b972c71e443effcb7317fa01eb2c63d724b2a4fb
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-05 23:58:48 +0530

    score added and movement done

--- Day_9_Snake_Game/app.js
+++ Day_9_Snake_Game/app.js
@@ -1,36 +1,85 @@
 let blockSize = 25;
 let rows = 20;
 let column = 20;
-
+let score = 0;
 let snakeX = 25;
 let snakeY = 25;
-let velocity = 1;
+let velocityX = 0;
+let velocityY = 0;
 
 let foodX = 125;
 let foodY = 125;
 
 let board = document.getElementById("board");
 let ctx = board.getContext("2d");
-board.width = column * blockSize;
-board.height = rows * blockSize;
 
 
-// game board
-ctx.fillStyle = "#1e1e1e";
-ctx.fillRect(0,0,board.width,board.height);
+window.onload= function(){
+    
+    // board configruration
+    board.width = column * blockSize;
+    board.height = rows * blockSize;
 
-// Snake
-ctx.fillStyle = "#57ef5d";
-ctx.fillRect(snakeX,snakeY,blockSize,blockSize);
+    
+    RandomFood()
+    document.addEventListener("keydown",changeDir);
 
+    setInterval(update,150);
+    // update()
+}
 
+function changeDir(e){
+    if(e.key  == "ArrowUp"  && velocityY!= 1 *blockSize){
+        console.log(e.key);
+        velocityY = -1 * blockSize;
+        velocityX = 0;
+    }
+    else if(e.key  == "ArrowDown"  && velocityY!= -1 *blockSize ){
+        console.log(e.key);
+        velocityY = 1 * blockSize;
+        velocityX = 0;
+    }
+    else if(e.key  == "ArrowLeft"  && velocityX!= 1 *blockSize){
+        console.log(e.key);
+        velocityY = 0;
+        velocityX = -1 * blockSize;
+    }
+    else if(e.key  == "ArrowRight"  && velocityX!= -1 *blockSize ){
+        console.log(e.key);
+        velocityY = 0;
+        velocityX = 1* blockSize;
+    }
+
+}
 
 function RandomFood(){
     
     foodX = Math.floor( Math.random() * rows) * blockSize;
     foodY = Math.floor( Math.random() * column) * blockSize;
+    // // Food
+    // ctx.fillStyle = "red";
+    // ctx.fillRect(foodX,foodY,blockSize,blockSize)
+}
+
+
+function update(){
+    console.log(velocityX, velocityY)
+    snakeX = snakeX + velocityX;
+    snakeY  = snakeY + velocityY;
+    // game board
+    ctx.fillStyle = "#1e1e1e";
+    ctx.fillRect(0,0,board.width,board.height);
+
     // Food
     ctx.fillStyle = "red";
     ctx.fillRect(foodX,foodY,blockSize,blockSize)
+
+    if( snakeX == foodX && snakeY == foodY){
+        score = score +1;
+        document.getElementById("score").innerText = score;
+        RandomFood()
+    }
+    // Snake
+    ctx.fillStyle = "#57ef5d";
+    ctx.fillRect(snakeX,snakeY,blockSize,blockSize);
 }
-RandomFood()
