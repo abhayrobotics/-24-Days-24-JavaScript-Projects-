2569b4f412db4520245a2b4d64c936b820716711
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-06 01:17:12 +0530

    game ready

--- Day_9_Snake_Game/app.js
+++ Day_9_Snake_Game/app.js
@@ -2,8 +2,12 @@ let blockSize = 25;
 let rows = 20;
 let column = 20;
 let score = 0;
+let gameover = false;
+
 let snakeX = 25;
 let snakeY = 25;
+let snakeBody = [];
+
 let velocityX = 0;
 let velocityY = 0;
 
@@ -14,72 +18,114 @@ let board = document.getElementById("board");
 let ctx = board.getContext("2d");
 
 
-window.onload= function(){
+window.onload = function () {
+   
     
-    // board configruration
-    board.width = column * blockSize;
-    board.height = rows * blockSize;
 
+        // board configruration
+        board.width = column * blockSize;
+        board.height = rows * blockSize;
+
+
+        RandomFood()
+        document.addEventListener("keydown", changeDir);
+
+        setInterval(update, 150);
+        // update()
     
-    RandomFood()
-    document.addEventListener("keydown",changeDir);
-
-    setInterval(update,150);
-    // update()
 }
 
-function changeDir(e){
-    if(e.key  == "ArrowUp"  && velocityY!= 1 *blockSize){
-        console.log(e.key);
+function changeDir(e) {
+    if (e.key == "ArrowUp" && velocityY != 1 * blockSize) {
+        // console.log(e.key);
         velocityY = -1 * blockSize;
         velocityX = 0;
     }
-    else if(e.key  == "ArrowDown"  && velocityY!= -1 *blockSize ){
-        console.log(e.key);
+    else if (e.key == "ArrowDown" && velocityY != -1 * blockSize) {
+        // console.log(e.key);
         velocityY = 1 * blockSize;
         velocityX = 0;
     }
-    else if(e.key  == "ArrowLeft"  && velocityX!= 1 *blockSize){
-        console.log(e.key);
+    else if (e.key == "ArrowLeft" && velocityX != 1 * blockSize) {
+        // console.log(e.key);
         velocityY = 0;
         velocityX = -1 * blockSize;
     }
-    else if(e.key  == "ArrowRight"  && velocityX!= -1 *blockSize ){
-        console.log(e.key);
+    else if (e.key == "ArrowRight" && velocityX != -1 * blockSize) {
+        // console.log(e.key);
         velocityY = 0;
-        velocityX = 1* blockSize;
+        velocityX = 1 * blockSize;
     }
 
 }
 
-function RandomFood(){
-    
-    foodX = Math.floor( Math.random() * rows) * blockSize;
-    foodY = Math.floor( Math.random() * column) * blockSize;
+function RandomFood() {
+
+    foodX = Math.floor(Math.random() * rows) * blockSize;
+    foodY = Math.floor(Math.random() * column) * blockSize;
     // // Food
     // ctx.fillStyle = "red";
     // ctx.fillRect(foodX,foodY,blockSize,blockSize)
 }
 
 
-function update(){
-    console.log(velocityX, velocityY)
+function update() {
+    if (gameover) {
+        // alert("Game Over! Score: ",score )
+        return;
+    }
+    // console.log(velocityX, velocityY)
     snakeX = snakeX + velocityX;
-    snakeY  = snakeY + velocityY;
+    snakeY = snakeY + velocityY;
     // game board
     ctx.fillStyle = "#1e1e1e";
-    ctx.fillRect(0,0,board.width,board.height);
+    ctx.fillRect(0, 0, board.width, board.height);
 
     // Food
     ctx.fillStyle = "red";
-    ctx.fillRect(foodX,foodY,blockSize,blockSize)
+    ctx.fillRect(foodX, foodY, blockSize, blockSize)
 
-    if( snakeX == foodX && snakeY == foodY){
-        score = score +1;
+     // gameover conitions boundary
+     if (snakeX > board.width || snakeY > board.height || snakeX < 0 || snakeY < 0) {
+        gameover = true;
+        console.log("game over", gameover);
+        alert("Game Over! Score: ",score )
+    }
+    // gameover conitions body eat
+    for (i in snakeBody) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameover = true; 
+            console.log("gamover",gameover);
+            alert("Game Over! Score: ",score )
+        }
+    }
+
+    // if food eats
+    if (snakeX == foodX && snakeY == foodY) {
+        score = score + 1;
+        snakeBody.push([foodX, foodY])
+        console.log(snakeBody)
         document.getElementById("score").innerText = score;
         RandomFood()
     }
+    // pushing the snake last position to 2nd last 
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+        // console.log(snakeBody[i])
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY]
+    }
     // Snake
     ctx.fillStyle = "#57ef5d";
-    ctx.fillRect(snakeX,snakeY,blockSize,blockSize);
+    ctx.fillStyle = "lime";
+    ctx.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    // snakeBody
+    for (i in snakeBody) {
+        ctx.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+        // console.log()
+
+    }
+   
 }
