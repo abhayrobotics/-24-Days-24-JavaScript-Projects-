c6270ca2097b26bef03572d279edd9866bef3552
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-05 23:11:22 +0530

    snake &  food genrated

--- Day_9_Snake_Game/app.js
+++ Day_9_Snake_Game/app.js
@@ -1,8 +1,13 @@
 let blockSize = 25;
 let rows = 20;
 let column = 20;
-let snakeX = 10;
-let snakeY = 10;
+
+let snakeX = 25;
+let snakeY = 25;
+let velocity = 1;
+
+let foodX = 125;
+let foodY = 125;
 
 let board = document.getElementById("board");
 let ctx = board.getContext("2d");
@@ -11,5 +16,21 @@ board.height = rows * blockSize;
 
 
 // game board
-ctx.fillStyle = "black";
+ctx.fillStyle = "#1e1e1e";
 ctx.fillRect(0,0,board.width,board.height);
+
+// Snake
+ctx.fillStyle = "#57ef5d";
+ctx.fillRect(snakeX,snakeY,blockSize,blockSize);
+
+
+
+function RandomFood(){
+    
+    foodX = Math.floor( Math.random() * rows) * blockSize;
+    foodY = Math.floor( Math.random() * column) * blockSize;
+    // Food
+    ctx.fillStyle = "red";
+    ctx.fillRect(foodX,foodY,blockSize,blockSize)
+}
+RandomFood()
