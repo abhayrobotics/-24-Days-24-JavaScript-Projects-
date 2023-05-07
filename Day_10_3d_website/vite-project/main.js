e140979f33d718c817debc26f1bb197d78e7dbe9
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-07 16:32:36 +0530

    basic 3d website ready

--- Day_10_3d_website/vite-project/main.js
+++ Day_10_3d_website/vite-project/main.js
@@ -12,8 +12,9 @@ const scene = new THREE.Scene();
 
 // create our object
 const geometry = new THREE.SphereGeometry(3,64,64);
+// const geometry = new THREE.TorusKnotGeometry( 2, 0.8, 100, 16 ); 
 const material = new THREE.MeshStandardMaterial({
-    color: "#00ff80",
+    color: "#0000ff",
     roughness:0.5,
     
 });
@@ -29,10 +30,32 @@ const sizes ={
 
 
 
-// light
-const light = new THREE.PointLight(0xfffff,1,100);
-light.position.set(0,10,10);
+//  point light
+const light = new THREE.PointLight(0xffffff,1,100);
+light.position.set(0,5,5);
 scene.add(light);
+// ambient light
+const light2 = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light2 );
+
+// light helper
+// const sphereSize = 1;
+// const pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
+// scene.add( pointLightHelper );
+
+// plane helper
+// const plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0.2 ), 3 );
+// const helper1 = new THREE.PlaneHelper( plane, 1, 0xffff00 );
+// scene.add( helper1 );
+
+// polar grid helper
+// const radius = 10;
+// const sectors = 16;
+// const rings = 8;
+// const divisions = 64;
+// const helper = new THREE.PolarGridHelper( radius, sectors, rings, divisions );
+// scene.add( helper );
+
 
 // camera
 const camera = new THREE.PerspectiveCamera(45,sizes.width/ sizes.height,0.1,100);
@@ -40,8 +63,12 @@ camera.position.z = 20;
 // camera.position.x = 20;
 scene.add(camera);
 
-// renderer
+// helper
+// const helper = new THREE.CameraHelper( camera );
+// scene.add( helper );
 
+
+// renderer
 const  canvas = document.querySelector('.webgl');
 const renderer  = new  THREE.WebGLRenderer({canvas});
 renderer.setSize(sizes.width, sizes.height);
