7a3d335de6b9f8ee1e6f8d25108e020b8930fd41
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-15 09:00:13 +0530

    playlist added

--- Day_11_Music_player/main.js
+++ Day_11_Music_player/main.js
@@ -5,19 +5,50 @@ let playlist = [
         poster: "./asset/img/1.jpg",
         song_name: "Love You Zindagi",
         artist: "Amit Trevedi | Dear Zindagi",
+        currentTrack: true,
     },
     {
         url: "./asset/audio/2.mp3",
         poster: "./asset/img/2.jpg",
-        song_name: "Love You Zindagi",
-        artist: "Amit Trevedi | Dear Zindagi",
+        song_name: "Kho gaye hum kaha",
+        artist: "Jasleen Kaur Royal",
+        currentTrack: false
+    },
+    {
+        url: "./asset/audio/3.mp3",
+        poster: "./asset/img/3.jpg",
+        song_name: "Samjhawahan",
+        artist: "Arijit Singh | Shreya Ghosal ",
+        currentTrack: false
+    },
+    {
+        url: "./asset/audio/4.mp3",
+        poster: "./asset/img/4.jpg",
+        song_name: "Yun hi ",
+        artist: "Mohit Chouhan ",
+        currentTrack: false
     },
 
 ]
-console.log(playlist[0])
-let currentTrack =0 ;
-let music = new Audio(playlist[currentTrack].url);
+// global valriable current trrack
+let track ;
+let poster= document.getElementsByClassName('music__poster');
+
+// console.log(poster,song_title,artist)
+
+
+for(item in playlist){
+    if (playlist[item].currentTrack == true){
+        console.log(playlist[item])
+        console.log(item)
+        track =item;
+
+    }
+}
+// console.log(track)
+
 // let music = new Audio("./asset/audio/1.mp3");
+let music = new Audio(playlist[track].url);
 let play = document.getElementById('play');
 let pause = document.getElementById('pause');
 
@@ -28,10 +59,13 @@ function Play() {
     play.classList.toggle('hide');
     pause.classList.toggle('hide');
 
+    
+
 }
-console.log(music.duration)
+// console.log(music.duration)
 
 function Pause() {
+    console.log("pause");
     music.pause();
     play.classList.toggle('hide');
     pause.classList.toggle('hide');
@@ -40,13 +74,37 @@ function Pause() {
 // next 
 function Next() {
     music.pause()
-    currentTrack =+1;
-    
-    music = new Audio ((playlist[currentTrack].url));
+    console.log("next");
+    track +=1;
+    update();
+    // changing current track
+    music = new Audio ((playlist[track].url));
+    music.play()
+}
+function Previous() {
+    music.pause()
+    console.log("next");
+    track -=1;
+    update();
+    // changing current track
+    music = new Audio ((playlist[track].url));
     music.play()
 }
 
 function Volume() {
     music.volume = music.volume - 0.1;
     console.log(music.volume)
+}
+
+function update(){
+    console.log("update");
+    track =parseInt(track);
+    console.log(playlist[track].poster)
+    
+    document.getElementById("song_name").innerText = playlist[track].song_name;
+    document.getElementById('artist').innerText =  playlist[track].artist;
+
+    let poster = document.getElementById('music__poster');
+    poster.style.backgroundImage =`url('${playlist[track].poster}')`
+    
 }
