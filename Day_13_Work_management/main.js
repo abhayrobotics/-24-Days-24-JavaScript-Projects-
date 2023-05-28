f7150b62ef1c36055bba3af80db1e5f6486890cf
abhaygupt <abhayrobotics1908@gmail.com>
2023-05-29 00:33:53 +0530

    two lists

--- Day_13_Work_management/main.js
+++ Day_13_Work_management/main.js
@@ -3,29 +3,59 @@
 let todo = document.getElementById('todo-area')
 new Sortable(todo, {
     animation: 150,
-    ghostClass: 'drag'
+    // ghostClass: 'drag'.
+    group:'drag'
+});
+let doing = document.getElementById('doing-area')
+new Sortable(doing, {
+    animation: 150,
+    group:'drag'
+    // ghostClass: 'drag'
 });
 
 //? add item
-function AddItem(){
+function AddItem() {
     let new_item = document.getElementById('todo-area');
     let add = document.getElementById('add')
     let text = add.previousElementSibling.value;
-    if(text != ""){
-        document.getElementById('todo-area').innerHTML = new_item.innerHTML + `  <div class="new-item drag">${text}</div>`
+    if (text != "") {
+        document.getElementById('todo-area').innerHTML = new_item.innerHTML +
+         ` <div class="new-item drag">
+              <div class="title">${text}</div>
+              <i class="ri-close-circle-fill delete " delete="true" ></i>
+          </div>`
 
     }
-    else{
-        console.log(text,"wrong")
+    else {
+        console.log(text, "wrong")
         let addBar = document.getElementById("card").innerHTML;
         // adding element 
         document.getElementById("card").innerHTML = addBar + ` <div class="warning">Card cannot be blank</div>`;
-// removing element
-        setTimeout(function(){
+        // removing element
+        setTimeout(function () {
             document.getElementById("card").innerHTML = addBar;
 
-        },2100)
+        }, 2100)
 
     }
+
+}
+
+// ? ***************cookie save
+// let cookie1 = document.getElementById('cookie')
+// cookie1.onclick = function () {
+//     console.log("cookie");
+//     document.cookie = "Cookie By = abhayRobotics; max-age=" + 300;
+// }
+
+// deleting
+document.onclick = function (e) {
     
+    // console.log(e.target)
+    // console.log(typeof e.target.getAttribute('delete'));
+    // remove parent element
+    if(e.target.getAttribute('delete') == "true"){
+
+        e.target.parentElement.remove();
+    }
 }
